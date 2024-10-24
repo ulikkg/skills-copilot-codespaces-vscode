69d8409946c7d9fddd2c6ead429ade14f1dc82f0
ulikkg <ulik.kg@gmail.com>
2024-10-24 19:59:42 +0200

    updated as it is needed

--- member.js
+++ member.js
@@ -3,6 +3,15 @@ function skillsMember() {
     name: 'John Doe',
     age: 25,
     skills: ['JS', 'CSS', 'HTML', 'PHP', 'Python', 'Ruby', 'Java']
+    
+    addSkill: function(skill) {
+      this.skills.push(skill);
+    }
+    //method to list all skills
+    listSkills: function() {
+      for (var i = 0; i < this.skills.length; i++) {
+        console.log(this.skills[i]);        
+  
   };
 
   return member;
